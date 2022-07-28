3be749cf7f8e899ff92fa118bd5697b5c600a5fb
manac <drploskikh@gmail.com>
2022-07-28 18:04:55 +0200

    Report actual self version

--- mwdstdb/api/meta.py
+++ mwdstdb/api/meta.py
@@ -1,4 +1,5 @@
 from typing import List
+import importlib
 
 from fastapi import APIRouter, Depends, HTTPException
 
@@ -12,6 +13,18 @@ app = APIRouter(
         ]
 )
 
+try:
+    self_version = importlib.metadata.version('mwdstdb')
+    self_description = importlib.metadata.metadata('mwdstdb')['Summary']
+except:
+    import tomlkit
+    with open('pyproject.toml') as pyproject:
+        file_contents = pyproject.read()
+
+    content = tomlkit.parse(file_contents)['tool']['poetry']
+    self_version = f'{content["version"]}-dev'
+    self_description = content['description']
+
 @app.get("/versions/", 
     tags=["meta"],
     summary="Get version information of known components",
@@ -21,7 +34,7 @@ async def get_versions(
         db = Depends(DBEngine.get_db), 
         ):
     return [
-        {'name': 'mwdstdb', 'version': '0.5.1', 'buildDate': 0, 'description': 'MWD STD Basic data storage and automation backend', 'homepage': 'https://github.com/mwdstd/mwdstdb'},
+        {'name': 'mwdstdb', 'version': self_version, 'buildDate': 0, 'description': self_description, 'homepage': 'https://github.com/mwdstd/mwdstdb'},
         {'name': 'mwdstdcore', 'version': '0.5.0', 'buildDate': 0, 'description': 'MWD STD Basic calculation server', 'homepage': 'https://github.com/mwdstd/mwdstdcore'},
         {'name': 'mwdstdwits', 'version': '0.5.0', 'buildDate': 0, 'description': 'MWD STD WITS Service', 'homepage': 'https://github.com/mwdstd/mwdstdwits'}
     ]
